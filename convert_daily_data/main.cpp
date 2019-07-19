0f079c04d31df718c283e94b1461a2f53bab0fb2
Dayong Zhou <dayong.zhou1999@gmail.com>
2019-07-19 11:22:13 -0500

    Initial Commit

---
+++ convert_daily_data/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  convert_daily_data
+//
+//  Created by Zhou, Dayong on 7/19/19.
+//  Copyright © 2019 Zhou, Dayong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
